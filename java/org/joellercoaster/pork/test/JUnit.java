7e80f6b32af1f283eb3f3edc05db41fc872ac2de
joel <joel.gluth@gmail.com>
2009-10-14 16:37:30 +0100

    Crudest of all possible JUnit runners

---
+++ java/org/joellercoaster/pork/test/JUnit.java
@@ -0,0 +1,25 @@
+package org.joellercoaster.pork ;
+
+import java.util.Enumeration;
+
+import junit.runner.SimpleTestCollector;
+import junit.runner.TestCollector;
+import junit.textui.TestRunner;
+
+public class JUnit
+{
+
+    public static void main(String[] argv)
+        throws ClassNotFoundException
+    {
+
+        TestRunner runner = new TestRunner();
+        TestCollector collector = new SimpleTestCollector();
+        for (Enumeration e = collector.collectTests() ; e.hasMoreElements() ; ) {
+            String name = (String) e.nextElement();
+            System.out.println(name);
+            runner.run(Class.forName(name));
+        }
+    }
+    
+}
