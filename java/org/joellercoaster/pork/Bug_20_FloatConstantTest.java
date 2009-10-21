3593ea50cec44dbade5afa74dd8f3db8ae8691b9
joel <joel.gluth@gmail.com>
2009-10-21 16:18:47 +0100

    Failing test demonstrating mangled CONSTANT_Float_info fields in the classfile

---
+++ java/org/joellercoaster/pork/Bug_20_FloatConstantTest.java
@@ -0,0 +1,17 @@
+package org.joellercoaster.pork;
+
+import junit.framework.TestCase;
+
+public class Bug_20_FloatConstantTest
+    extends TestCase
+{
+
+    public Bug_20_FloatConstantTest(String name) {
+        super(name);
+    }
+
+    public void testFloatLiteral() {
+        assertEquals(0.1, FloatConstant.returnPointOne());
+    }
+
+}
